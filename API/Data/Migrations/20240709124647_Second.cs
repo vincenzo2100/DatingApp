a7e0f5e5f385b3cb72afbb030329086dbaf41e62
vincenzo2100 <vincenzo2100@wp.pl>
2024-07-09 15:02:25 +0200

    Section 2

---
+++ API/Data/Migrations/20240709124647_Second.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_appUsers",
+                table: "appUsers");
+
+            migrationBuilder.RenameTable(
+                name: "appUsers",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "appUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_appUsers",
+                table: "appUsers",
+                column: "Id");
+        }
+    }
+}
