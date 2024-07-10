39158f590b7e21aa3fdb7dfd62f82cbca9ca36f2
vincenzo2100 <vincenzo2100@wp.pl>
2024-07-10 13:45:58 +0200

    Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,18 @@ export class AppComponent implements OnInit {
   title = 'The dating app';
   users :any;
 
-  constructor(private http: HttpClient){
+  constructor(private accountService: AccountService){
 
   }
 
   ngOnInit() {
-   this.getUsers();
+   this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('http://localhost:5034/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
+  
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+  model:any = {};
+
+  constructor(private accountService: AccountService){
+
+  }
+
+  register(){
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    },error => console.log(error))
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+
+}
