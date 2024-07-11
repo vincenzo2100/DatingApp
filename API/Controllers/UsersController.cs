12778b093473af58a9c68fcf0890fef4f83904f1
vincenzo2100 <vincenzo2100@wp.pl>
2024-07-11 20:13:40 +0200

    Section 8 half

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   
+   [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -22,13 +22,13 @@ namespace API.Controllers
 
 
         [HttpGet]
-        [AllowAnonymous]
+       
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
+       
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
