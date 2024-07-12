79d897858c269d96c5b8be4f1acd50b93ddb285b
vincenzo2100 <vincenzo2100@wp.pl>
2024-07-12 16:40:59 +0200

    Section 9

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
 
 
 
@@ -16,12 +18,14 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     positionClass: 'toast-bottom-right'
     }),
     TabsModule.forRoot(),
+    NgxGalleryModule
   
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
     TabsModule,
+    NgxGalleryModule
  
   ]
 })
